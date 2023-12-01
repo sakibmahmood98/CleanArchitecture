6792d5b21c88c8e0e5ddaca9e7a8cee641fefdac
Sakib Mahmood <sakib.mahmood@brainstation-23.com>
2023-12-01 16:38:40 +0600

    add db provider

--- Store.Infrastructure/Persistence/Contexts/StoreContext.cs
+++ Store.Infrastructure/Persistence/Contexts/StoreContext.cs
@@ -1,11 +1,22 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Store.ApplicationCore.Entities;
+
 namespace Store.Infrastructure.Persistence.Contexts;
 
 public class StoreContext: DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))
+                .Build();
+            var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(defaultConnectionString);
+        }
+    }
     public StoreContext(DbContextOptions<StoreContext> options) : base(options)
     {
 
